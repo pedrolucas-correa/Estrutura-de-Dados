8630fd21b7e21c0d4bfe84b812f90fe382366d11
Pedro Lucas Cardoso Correa <69874110+pedrolucas-correa@users.noreply.github.com>
2021-09-05 12:45:30 -0300

    Update 01 - Temperatura Celsius para Fahrenheit.c

--- Atividade 1/01 - Temperatura Celsius para Fahrenheit.c
+++ Atividade 1/01 - Temperatura Celsius para Fahrenheit.c
@@ -4,7 +4,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main() {
 	float c, f;
 	
 	printf("Digite a temperatura em Celsius: ");
